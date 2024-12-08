d70164edac7599615a80bc39e0b33d8a69aa5241
deyzar <deyzar00@mail.ru>
2024-12-08 19:18:51 +0300

    Описал первый роутинг и контроллер пользователей + создал пустой контроллер для задач

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,23 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root 'tasks#index'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/profile', to: 'users#show'
+
+  resources :tasks do
+    member do
+      get 'delete'
+    end
+  end
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
