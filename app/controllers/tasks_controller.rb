3e3ed8535cc969f3fdbc86bd283015ced0954eba
deyzar <deyzar00@mail.ru>
2024-12-08 23:20:31 +0300

    Rubocop fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,6 @@ class TasksController < ApplicationController
   end
 
   def show
-
   end
 
   def new
@@ -25,7 +24,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
