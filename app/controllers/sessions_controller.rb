79f6db23feb959ee48038b995f95b5264e7954b4
deyzar <deyzar00@mail.ru>
2024-12-08 19:40:49 +0300

    Описал контроллер для сессий и некоторые другие правки

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    # Здесь Леша напишет код для формы для входа
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to tasks_path, notice: 'Вы вошли в систему!'
+    else
+      flash.now[:alert] = 'Неверное имя пользователя или пароль.'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Вы вышли из системы.'
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_hash)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
