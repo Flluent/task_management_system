28db024f3195095dfcbcfc76323deaed90d03200
Semechka <utsushka@yandex.ru>
2024-12-08 20:05:41 +0300

    Большое обновление: вместе с Сережей и Денисом сделали правильную регистрацию и авторизацию (пришлось сделать некоторую новую миграцию)

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users, only: [:new, :create]
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_08_155822) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_08_165132) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,7 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_08_155822) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_hash"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
